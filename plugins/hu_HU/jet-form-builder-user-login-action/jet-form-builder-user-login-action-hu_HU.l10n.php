9ad3d1fc474289df0a61a48acde1cb1e02c48c06
trueqap <trueqap@gmail.com>
2025-10-20 09:00:41 +0200

    Add Hungarian translations for 25 additional plugins
    New plugins added:
    - automatewoo
    - checkout-wc
    - complianz-gdpr
    - customize-my-account-page-for-woocommerce
    - dynamic-content-for-elementor
    - fluent-booking-pro
    - fluent-cart-pro
    - fluent-support-pro
    - jet-form-builder-user-login-action
    - jet-tricks
    - learndash-notifications
    - learndash-woocommerce
    - mainwp (main plugin)
    - mainwp-code-snippets-extension
    - mainwp-google-analytics-extension
    - tutor-pro
    - updraftplus
    - woocommerce-cost-of-goods
    - woocommerce-memberships
    - woocommerce-product-feeds
    - woocommerce-product-reviews-pro
    - woocommerce-shipping-local-pickup-plus
    - wp-all-import-pro
    - wp-booking-system-authorize-net
    - wp-booking-system-search
    
    All translations include generated MO and L10N.php files.
    Total: 181 plugin translations in the repository.

---
+++ plugins/hu_HU/jet-form-builder-user-login-action/jet-form-builder-user-login-action-hu_HU.l10n.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	'project-id-version' => 'JetFormBuilder User Login Action 2.0.6',
+	'report-msgid-bugs-to' => 'support@hellowp.io',
+	'po-revision-date' => '2025-10-19 22:48',
+	'last-translator' => 'HelloWP <translate@hellowp.io>',
+	'language-team' => 'HelloWP <translate@hellowp.io>',
+	'language' => 'hu',
+	'mime-version' => '1.0',
+	'content-type' => 'text/plain; charset=utf-8',
+	'content-transfer-encoding' => '8bit',
+	'plural-forms' => 'nplurals=2; plural=(n != 1);',
+	'x-generator' => 'sinann-po-translator',
+	'x-plugin-name' => 'JetFormBuilder User Login Action',
+	'x-plugin-version' => '2.0.6',
+	'messages' => [
+		'JetFormBuilder User Login Action' => 'JetFormBuilder felhasználói bejelentkezési művelet',
+		'https://jetformbuilder.com/addons/user-login/' => 'https://jetformbuilder.com/addons/user-login/',
+		'A post-submit action addon allowing users to log in by the provided email/login & password.' => 'Egy elküldés utáni műveleti kiegészítő, amely lehetővé teszi a felhasználóknak, hogy a megadott e-mail/felhasználónév & jelszóval bejelentkezzenek.',
+		'Crocoblock' => 'Crocoblock',
+		'https://crocoblock.com/' => 'https://crocoblock.com/',
+	],
+];
---
+++ plugins/hu_HU/woocommerce-product-feeds/woocommerce-product-feeds-hu_HU.l10n.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	'project-id-version' => 'WooCommerce Google Product Feed 11.0.25',
+	'report-msgid-bugs-to' => 'support@hellowp.io',
+	'po-revision-date' => '2025-10-19 22:50',
+	'last-translator' => 'HelloWP <translate@hellowp.io>',
+	'language-team' => 'HelloWP <translate@hellowp.io>',
+	'language' => 'hu',
+	'mime-version' => '1.0',
+	'content-type' => 'text/plain; charset=utf-8',
+	'content-transfer-encoding' => '8bit',
+	'plural-forms' => 'nplurals=2; plural=(n != 1);',
+	'x-generator' => 'sinann-po-translator',
+	'x-plugin-name' => 'WooCommerce Google Product Feed',
+	'x-plugin-version' => '11.0.25',
+	'messages' => [
+		'WooCommerce Google Product Feed' => 'WooCommerce Google Product Feed',
+		'https://woocommerce.com/products/google-product-feed/' => 'https://woocommerce.com/products/google-product-feed/',
+		'WooCommerce extension that allows you to more easily populate advanced attributes into the Google Merchant Centre feed' => 'WooCommerce bővítmény, amely megkönnyíti a speciális attribútumok feltöltését a Google Merchant Centre feedbe',
+		'Ademti Software Ltd.' => 'Ademti Software Ltd.',
+		'https://www.ademti-software.co.uk/' => 'https://www.ademti-software.co.uk/',
+	],
+];
